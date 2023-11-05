1c81dd479a5ac0ca65c2a7a06b20c4a8a78928c8
Asha Gandhudi <agandhudi@gmail.com>
2023-11-05 04:29:51 -0800

    first commit for project

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,14 @@
+package SolarSystem;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        solarSystemBasics solarNew = new solarSystemBasics();
+        solarNew.Sun = "1";
+        solarNew.Moon = "2";
+        solarNew.Planets = "2";
+        solarNew.Stars = "10";
+
+    }
+}
